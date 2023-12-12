38a230577e0285930e7b7da5c38b6fe763baf263
MorrisCastro <jomarcastro2004@gmail.com>
2023-12-12 17:32:17 -0400

    imprime $ para que la persona escriba su comando

--- main.c
+++ main.c
@@ -1,12 +1,23 @@
 #include "shell.h"
 
 /**
- * 
- * 
- * 
+ *
+ *
+ *
  */
 int main(int ac __attribute__((unused)), char **av)
 {
-    printArguments(av);
+    char *buffer = NULL;
+    size_t n = 0;
+
+    (void)av;
+    while (1)
+    {
+        /*verifica si la entrada esta asociada a un terminal*/
+        if (isatty(0))
+            write(1, "$ ", 2);
+        /*lee la lina desde stdin y lo almacena en buffer*/
+        getline(&buffer, &n, stdin);
+    }
     return (0);
 }
