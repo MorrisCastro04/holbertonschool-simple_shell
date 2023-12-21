9532804b2f39b65075cb636746f2994901ebe38f
MorrisCastro <jomarcastro2004@gmail.com>
2023-12-20 21:32:40 -0400

    fix the memory leak in the exit with a for loop

--- buildin.c
+++ buildin.c
@@ -15,9 +15,21 @@ void print_env(char **env)
         write(1, "\n", 1);
     }
 }
+/**
+ * 
+ * 
+ * 
+ * 
+*/
 int ifexit(char **tokens)
 {
+    int i;
+
     if (strcmp(tokens[0], "exit") == 0)
+    {
+        for (i = 0; tokens[i] != NULL; i++)
+            free(tokens[i]);
         exit(0);
+    }
     return(1);
 }
